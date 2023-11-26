967954f0d6a07e45b8eb10d9bae00b549dd53f51
Vivan-jha <vivaanjha2002@gmail.com>
2023-11-26 12:57:26 +0530

    commit

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -1,55 +1,108 @@
-// SPDX-License-Identifier: BUSL-1.1
-pragma solidity 0.8.16;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
 
-import {SafeERC20Upgradeable, IERC20Upgradeable} from "@openzeppelin/contracts-upgradeable-4.3.2/token/ERC20/utils/SafeERC20Upgradeable.sol";
-import {Initializable} from "@openzeppelin/contracts-upgradeable-4.3.2/proxy/utils/Initializable.sol";
-import {ExchangeData} from "../ExternalSwapHandler/Helper/ExchangeData.sol";
-import {TransferHelper} from "@uniswap/lib/contracts/libraries/TransferHelper.sol";
-import {ErrorLibrary} from "./../../library/ErrorLibrary.sol";
-import {ApproveControl} from "../ApproveControl.sol";
-import {IPriceOracle} from "../../oracle/IPriceOracle.sol";
-import {ExternalSlippageControl} from "../ExternalSlippageControl.sol";
+// A partial ERC20 interface.
+interface IERC20 {
+    function balanceOf(address owner) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transfer(address to, uint256 amount) external returns (bool);
+}
 
-contract BebopHandler is Initializable, ApproveControl, ExternalSlippageControl {
-  using SafeERC20Upgradeable for IERC20Upgradeable;
-  address internal swapTarget;
-  IPriceOracle internal oracle;
+// A partial WETH interfaec.
+interface IWETH is IERC20 {
+    function deposit() external payable;
+}
 
-  function init(address _swapTarget, address _oracle) external initializer {
-    swapTarget = _swapTarget;
-    oracle = IPriceOracle(_oracle);
-  }
+// Demo contract that swaps its ERC20 balance for another ERC20.
+// NOT to be used in production.
+contract SimpleTokenSwap {
 
-  function swap(
-    address sellTokenAddress,
-    address buyTokenAddress,
-    uint256 sellAmount,
-    bytes memory callData,
-    address _to
-  ) public payable {
-    uint256 tokenBalance = IERC20Upgradeable(sellTokenAddress).balanceOf(address(this));
-    if (tokenBalance < sellAmount) {
-      revert ErrorLibrary.InsufficientFunds(tokenBalance, sellAmount);
+    event BoughtTokens(IERC20 sellToken, IERC20 buyToken, uint256 boughtAmount);
+
+    // The WETH contract.
+    IWETH public immutable WETH;
+    // Creator of this contract.
+    address public owner;
+    // 0x ExchangeProxy address.
+    // See https://docs.0x.org/developer-resources/contract-addresses
+    address public exchangeProxy;
+
+    constructor(IWETH _weth, address _exchangeProxy) {
+        WETH = _weth;
+        exchangeProxy = _exchangeProxy;
+        owner = msg.sender;
     }
-    setAllowance(sellTokenAddress, swapTarget, sellAmount);
-    uint256 tokensBefore = IERC20Upgradeable(buyTokenAddress).balanceOf(address(this));
-    (bool success, ) = swapTarget.call(callData);
-    if (!success) {
-      revert ErrorLibrary.SwapFailed();
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "ONLY_OWNER");
+        _;
     }
-    uint256 tokensSwapped;
 
-    uint buyTokenBalance = IERC20Upgradeable(buyTokenAddress).balanceOf(address(this));
-    tokensSwapped = buyTokenBalance - tokensBefore;
-    if (tokensSwapped == 0) {
-      revert ErrorLibrary.ZeroTokensSwapped();
+    // Payable fallback to allow this contract to receive protocol fee refunds.
+    receive() external payable {}
+
+    // Transfer tokens held by this contrat to the sender/owner.
+    function withdrawToken(IERC20 token, uint256 amount)
+        external
+        onlyOwner
+    {
+        require(token.transfer(msg.sender, amount));
     }
-    uint priceSellToken = oracle.getPriceTokenUSD18Decimals(sellTokenAddress, sellAmount);
-    uint priceBuyToken = oracle.getPriceTokenUSD18Decimals(buyTokenAddress, buyTokenBalance);
 
-    validateSwap(priceSellToken, priceBuyToken);
-    TransferHelper.safeTransfer(buyTokenAddress, _to, IERC20Upgradeable(buyTokenAddress).balanceOf(address(this)));
-  }
+    // Transfer ETH held by this contrat to the sender/owner.
+    function withdrawETH(uint256 amount)
+        external
+        onlyOwner
+    {
+        payable(msg.sender).transfer(amount);
+    }
 
-  receive() external payable {}
+    // Transfer ETH into this contract and wrap it into WETH.
+    function depositETH()
+        external
+        payable
+    {
+        WETH.deposit{value: msg.value}();
+    }
+
+    // Swaps ERC20->ERC20 tokens held by this contract using a 0x-API quote.
+    function fillQuote(
+        // The `sellTokenAddress` field from the API response.
+        IERC20 sellToken,
+        // The `buyTokenAddress` field from the API response.
+        IERC20 buyToken,
+        // The `allowanceTarget` field from the API response.
+        address spender,
+        // The `to` field from the API response.
+        address payable swapTarget,
+        // The `data` field from the API response.
+        bytes calldata swapCallData
+    )
+        external
+        onlyOwner
+        payable // Must attach ETH equal to the `value` field from the API response.
+    {
+        // Checks that the swapTarget is actually the address of 0x ExchangeProxy
+        require(swapTarget == exchangeProxy, "Target not ExchangeProxy");
+
+        // Track our balance of the buyToken to determine how much we've bought.
+        uint256 boughtAmount = buyToken.balanceOf(address(this));
+
+        // Give `spender` an infinite allowance to spend this contract's `sellToken`.
+        // Note that for some tokens (e.g., USDT, KNC), you must first reset any existing
+        // allowance to 0 before being able to update it.
+        require(sellToken.approve(spender, type(uint256).max));
+        // Call the encoded swap function call on the contract at `swapTarget`,
+        // passing along any ETH attached to this function call to cover protocol fees.
+        (bool success,) = swapTarget.call{value: msg.value}(swapCallData);
+        require(success, 'SWAP_CALL_FAILED');
+        // Refund any unspent protocol fees to the sender.
+        // Refund any unspent protocol fees to the sender.
+payable(msg.sender).transfer(address(this).balance);
+
+
+        // Use our current buyToken balance to determine how much we've bought.
+        boughtAmount = buyToken.balanceOf(address(this)) - boughtAmount;
+        emit BoughtTokens(sellToken, buyToken, boughtAmount);
+    }
 }
